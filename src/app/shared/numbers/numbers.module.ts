99d0de95d0282b28c857515d597d66fee24d57ae
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-01-17 12:17:05 -0300

    Creados módulos Lazy Loaded

---
+++ src/app/shared/numbers/numbers.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+
+
+
+@NgModule({
+  declarations: [
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
+  ]
+})
+export class NumbersModule { }
